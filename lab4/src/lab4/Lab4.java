4061658cb4495f5b8e583ebd0312070b1a6bc576
Estaniablanc <71726093+Estaniablanc@users.noreply.github.com>
2021-03-09 14:08:11 -0500

    Update Lab4.java

--- lab4/src/lab4/Lab4.java
+++ lab4/src/lab4/Lab4.java
@@ -6,12 +6,12 @@ public class Lab4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double num1, num2, num3;
+		float num1, num2, num3;
 		Scanner in = new Scanner(System.in);
         System.out.println("please enter three numbers: ");
-        num1 = in.nextDouble();
-        num2 = in.nextDouble();
-        num3 = in.nextDouble();
+        num1 = in.nextFloat();
+        num2 = in.nextFloat();
+        num3 = in.nextFloat();
         System.out.print("The Largest number is: ");
         if (num1 >= num2 && num1 >= num3)
             System.out.println(num1 +".");
