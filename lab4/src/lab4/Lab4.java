ed335f9f552f9415b297ae30b26087181df057ad
Estaniablanc <71726093+Estaniablanc@users.noreply.github.com>
2021-03-09 14:11:04 -0500

    Update Lab4.java

--- lab4/src/lab4/Lab4.java
+++ lab4/src/lab4/Lab4.java
@@ -14,12 +14,12 @@ public class Lab4 {
         num3 = in.nextFloat();
         System.out.print("The Largest number is: ");
         if (num1 >= num2 && num1 >= num3)
-            System.out.println(num1 +".");
+            System.out.println(num1);
         
 	    else if (num2 >= num3)
-            System.out.println(num2+".");
+            System.out.println(num2);
         else
-            System.out.println(num3+".");
+            System.out.println(num3);
          
         }
 	}	 
